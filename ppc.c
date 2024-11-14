484f4302789354f0d7d906c904170d5966336011
OmarPP03 <omarpasalic@gmail.com>
2024-11-14 12:44:12 +0100

    Solved Task 2.

--- ppc.c
+++ ppc.c
@@ -19,25 +19,15 @@ bool checkSym(float** matrix, int size) {
 
 bool checkSymImp(float** matrix, int size) {
   for (int i = 0; i < size; i++) {
-    int j;
-
-    // Vectorized comparison for chunks of 8 elements
-    for (j = 0; j <= i - 8; j += 8) {
-      __m256 row_data = _mm256_loadu_ps(&matrix[i][j]);
-      __m256 col_data = _mm256_loadu_ps(&matrix[j][i]);
-      __m256 cmp = _mm256_cmp_ps(row_data, col_data, _CMP_NEQ_UQ);
-
-      // If any elements are unequal, return false
-      if (!_mm256_testz_ps(cmp, cmp)) {
-        return false;
-      }
-    }
-
-    // Scalar fallback for remaining elements if `i` is not a multiple of 8
-    for (; j < i; j++) {
-      if (matrix[i][j] != matrix[j][i]) {
-        return false;
-      }
+    #pragma ivdep
+    #pragma GCC unroll(4)
+    for (int j = 0; j <= i - 8; j += 8) {
+       _mm_prefetch((const char*)&matrix[i][j+8], _MM_HINT_T0); //Pre-load data into L1
+       _mm_prefetch((const char*)&matrix[j+8][i], _MM_HINT_T0);
+      __m256 row = _mm256_loadu_ps(&matrix[i][j]);
+      __m256 col = _mm256_loadu_ps(&matrix[j][i]);
+      __m256 cmp = _mm256_cmp_ps(row, col, _CMP_NEQ_UQ);
+      if (!_mm256_testz_ps(cmp, cmp)) return false;
     }
   }
   return true;
@@ -82,16 +72,20 @@ float** matTransposeImp(float** matrix, int size) {
     trans[i] = &data[i * size];
   }
   
-
-  for (int i = 0; i < size; i += 8) {          // Process 8 rows at a time
-    for (int j = 0; j < size; j++) {          // Process each column in these 8 rows
-      // Load 8 elements from the i-th row to (i+7)-th row at column j
-      __m256 row_data = _mm256_set_ps(
-        matrix[i+7][j], matrix[i+6][j], matrix[i+5][j], matrix[i+4][j],
-        matrix[i+3][j], matrix[i+2][j], matrix[i+1][j], matrix[i][j]
-      );
-
-      // Store transposed elements into `trans[j][i]` to `trans[j][i+7]`
+  #pragma ivdep 
+  #pragma unroll(4)
+  for (int i = 0; i < size; i += 8) {          
+    for (int j = 0; j < size; j++) {       
+      _mm_prefetch((const char*)&matrix[i + 8][j], _MM_HINT_T0);
+      _mm_prefetch((const char*)&trans[j][i + 8], _MM_HINT_T0);
+      __m256 row_data = _mm256_set_ps( matrix[i+7][j], 
+                                       matrix[i+6][j], 
+                                       matrix[i+5][j], 
+                                       matrix[i+4][j],
+                                       matrix[i+3][j], 
+                                       matrix[i+2][j], 
+                                       matrix[i+1][j], 
+                                       matrix[i][j] );
       _mm256_storeu_ps(&trans[j][i], row_data);
     }
   }
